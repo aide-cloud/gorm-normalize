2c6677a30bd60f7caea925158c9f7eb2e91534dd
胡标 <biao.hu@baishan.com>
2023-10-13 14:08:41 +0800

    增加新的业务方法, 增加关联操作

--- query.go
+++ query.go
@@ -17,17 +17,17 @@ type IOpration[T any] interface {
 	// FirstWithTrashed 查询单条数据(包含软删除数据)
 	FirstWithTrashed(wheres ...Scopemethod) (*T, error)
 	// FirstByID 根据ID查询单条数据
-	FirstByID(id uint) (*T, error)
+	FirstByID(id uint, wheres ...Scopemethod) (*T, error)
 	// FirstByIDWithTrashed 根据ID查询单条数据(包含软删除数据)
-	FirstByIDWithTrashed(id uint) (*T, error)
+	FirstByIDWithTrashed(id uint, wheres ...Scopemethod) (*T, error)
 	// Last 查询单条数据
 	Last(wheres ...Scopemethod) (*T, error)
 	// LastWithTrashed 查询单条数据(包含软删除数据)
 	LastWithTrashed(wheres ...Scopemethod) (*T, error)
 	// LastByID 根据ID查询单条数据
-	LastByID(id uint) (*T, error)
+	LastByID(id uint, wheres ...Scopemethod) (*T, error)
 	// LastByIDWithTrashed 根据ID查询单条数据(包含软删除数据)
-	LastByIDWithTrashed(id uint) (*T, error)
+	LastByIDWithTrashed(id uint, wheres ...Scopemethod) (*T, error)
 	// List 查询多条数据
 	List(pgInfo Pagination, wheres ...Scopemethod) ([]*T, error)
 	// ListWithTrashed 查询多条数据(包含软删除数据)
@@ -41,15 +41,61 @@ type IOpration[T any] interface {
 	// Update 更新数据
 	Update(m *T, wheres ...Scopemethod) error
 	// UpdateByID 根据ID更新数据
-	UpdateByID(id uint, m *T) error
+	UpdateByID(id uint, m *T, wheres ...Scopemethod) error
 	// Delete 删除数据
 	Delete(wheres ...Scopemethod) error
 	// DeleteByID 根据ID删除数据
-	DeleteByID(id uint) error
+	DeleteByID(id uint, wheres ...Scopemethod) error
 	// ForcedDelete 强制删除数据
 	ForcedDelete(wheres ...Scopemethod) error
 	// ForcedDeleteByID 根据ID强制删除数据
-	ForcedDeleteByID(id uint) error
+	ForcedDeleteByID(id uint, wheres ...Scopemethod) error
+}
+
+type AssociationKey string
+
+type IAssociation interface {
+	// Append 添加关联
+	Append(associationKey AssociationKey, list ...schema.Tabler) error
+	// Replace 替换关联
+	Replace(associationKey AssociationKey, list ...schema.Tabler) error
+	// Delete 删除关联
+	Delete(associationKey AssociationKey, list ...schema.Tabler) error
+	// Clear 清除关联
+	Clear(associationKey AssociationKey) error
+	// Count 关联数量
+	Count(associationKey AssociationKey) int64
+}
+
+type defaultAssociation struct {
+	db *gorm.DB
+}
+
+func (l *defaultAssociation) Append(associationKey AssociationKey, list ...schema.Tabler) error {
+	return l.db.Association(string(associationKey)).Append(list)
+}
+
+func (l *defaultAssociation) Replace(associationKey AssociationKey, list ...schema.Tabler) error {
+	return l.db.Association(string(associationKey)).Replace(list)
+}
+
+func (l *defaultAssociation) Delete(associationKey AssociationKey, list ...schema.Tabler) error {
+	return l.db.Association(string(associationKey)).Delete(list)
+}
+
+func (l *defaultAssociation) Clear(associationKey AssociationKey) error {
+	return l.db.Association(string(associationKey)).Clear()
+}
+
+func (l *defaultAssociation) Count(associationKey AssociationKey) int64 {
+	return l.db.Association(string(associationKey)).Count()
+}
+
+// NewDefaultAssociation 创建默认关联
+func NewDefaultAssociation(db *gorm.DB) IAssociation {
+	return &defaultAssociation{
+		db: db,
+	}
 }
 
 type IBind[T any] interface {
@@ -85,12 +131,15 @@ type IAction[T any] interface {
 	IBind[T]
 
 	DB() *gorm.DB
+	Association() IAssociation
 }
 
 type Action[T any] struct {
 	db  *gorm.DB
 	ctx context.Context
 	schema.Tabler
+
+	association IAssociation
 
 	// 开启trace
 	enableTrace bool
@@ -110,6 +159,8 @@ func NewAction[T any](opts ...ActionOption[T]) *Action[T] {
 	if action.Tabler != nil {
 		action.db = action.db.Table(action.Tabler.TableName())
 	}
+
+	action.association = NewDefaultAssociation(action.db)
 
 	return &action
 }
@@ -140,6 +191,10 @@ func WithTabler[T any](tabler schema.Tabler) ActionOption[T] {
 	return func(a *Action[T]) {
 		a.Tabler = tabler
 	}
+}
+
+func (a *Action[T]) Association() IAssociation {
+	return a.association
 }
 
 func (a *Action[T]) OpenTrace() IAction[T] {
@@ -241,7 +296,7 @@ func (a *Action[T]) FirstWithTrashed(wheres ...Scopemethod) (*T, error) {
 }
 
 // FirstByID 根据ID查询单条数据
-func (a *Action[T]) FirstByID(id uint) (*T, error) {
+func (a *Action[T]) FirstByID(id uint, wheres ...Scopemethod) (*T, error) {
 	ctx := a.ctx
 	if a.enableTrace {
 		_ctx, span := otel.Tracer("gorm-normalize").Start(a.ctx, "FirstByID")
@@ -249,7 +304,7 @@ func (a *Action[T]) FirstByID(id uint) (*T, error) {
 		ctx = _ctx
 	}
 	var m T
-	if err := a.DB().WithContext(ctx).First(&m, id).Error; err != nil {
+	if err := a.DB().WithContext(ctx).Scopes(wheres...).First(&m, id).Error; err != nil {
 		return nil, err
 	}
 
@@ -257,7 +312,7 @@ func (a *Action[T]) FirstByID(id uint) (*T, error) {
 }
 
 // FirstByIDWithTrashed 根据ID查询单条数据(包含软删除数据)
-func (a *Action[T]) FirstByIDWithTrashed(id uint) (*T, error) {
+func (a *Action[T]) FirstByIDWithTrashed(id uint, wheres ...Scopemethod) (*T, error) {
 	ctx := a.ctx
 	if a.enableTrace {
 		_ctx, span := otel.Tracer("gorm-normalize").Start(a.ctx, "FirstByIDWithTrashed")
@@ -265,7 +320,7 @@ func (a *Action[T]) FirstByIDWithTrashed(id uint) (*T, error) {
 		ctx = _ctx
 	}
 	var m T
-	if err := a.DB().WithContext(ctx).Unscoped().First(&m, id).Error; err != nil {
+	if err := a.DB().WithContext(ctx).Unscoped().Scopes(wheres...).First(&m, id).Error; err != nil {
 		return nil, err
 	}
 
@@ -307,7 +362,7 @@ func (a *Action[T]) LastWithTrashed(wheres ...Scopemethod) (*T, error) {
 }
 
 // LastByID 根据ID查询单条数据
-func (a *Action[T]) LastByID(id uint) (*T, error) {
+func (a *Action[T]) LastByID(id uint, wheres ...Scopemethod) (*T, error) {
 	ctx := a.ctx
 	if a.enableTrace {
 		_ctx, span := otel.Tracer("gorm-normalize").Start(a.ctx, "LastByID")
@@ -316,7 +371,7 @@ func (a *Action[T]) LastByID(id uint) (*T, error) {
 	}
 	var m T
 
-	if err := a.DB().WithContext(ctx).Last(&m, id).Error; err != nil {
+	if err := a.DB().WithContext(ctx).Scopes(wheres...).Last(&m, id).Error; err != nil {
 		return nil, err
 	}
 
@@ -324,7 +379,7 @@ func (a *Action[T]) LastByID(id uint) (*T, error) {
 }
 
 // LastByIDWithTrashed 根据ID查询单条数据(包含软删除数据)
-func (a *Action[T]) LastByIDWithTrashed(id uint) (*T, error) {
+func (a *Action[T]) LastByIDWithTrashed(id uint, wheres ...Scopemethod) (*T, error) {
 	ctx := a.ctx
 	if a.enableTrace {
 		_ctx, span := otel.Tracer("gorm-normalize").Start(a.ctx, "LastByIDWithTrashed")
@@ -333,7 +388,7 @@ func (a *Action[T]) LastByIDWithTrashed(id uint) (*T, error) {
 	}
 	var m T
 
-	if err := a.DB().WithContext(ctx).Unscoped().Last(&m, id).Error; err != nil {
+	if err := a.DB().WithContext(ctx).Unscoped().Scopes(wheres...).Last(&m, id).Error; err != nil {
 		return nil, err
 	}
 
@@ -451,14 +506,14 @@ func (a *Action[T]) Update(newModel *T, wheres ...Scopemethod) error {
 }
 
 // UpdateByID 根据ID更新数据
-func (a *Action[T]) UpdateByID(id uint, newModel *T) error {
+func (a *Action[T]) UpdateByID(id uint, newModel *T, wheres ...Scopemethod) error {
 	ctx := a.ctx
 	if a.enableTrace {
 		_ctx, span := otel.Tracer("gorm-normalize").Start(a.ctx, "UpdateByID")
 		defer span.End()
 		ctx = _ctx
 	}
-	return a.DB().WithContext(ctx).Where("id = ?", id).Updates(newModel).Error
+	return a.DB().WithContext(ctx).Scopes(append(wheres, WhereID(id))...).Updates(newModel).Error
 }
 
 // Delete 删除数据
@@ -474,7 +529,7 @@ func (a *Action[T]) Delete(wheres ...Scopemethod) error {
 }
 
 // DeleteByID 根据ID删除数据
-func (a *Action[T]) DeleteByID(id uint) error {
+func (a *Action[T]) DeleteByID(id uint, wheres ...Scopemethod) error {
 	ctx := a.ctx
 	if a.enableTrace {
 		_ctx, span := otel.Tracer("gorm-normalize").Start(a.ctx, "DeleteByID")
@@ -482,7 +537,7 @@ func (a *Action[T]) DeleteByID(id uint) error {
 		ctx = _ctx
 	}
 	var m T
-	return a.DB().WithContext(ctx).Delete(&m, id).Error
+	return a.DB().WithContext(ctx).Scopes(wheres...).Delete(&m, id).Error
 }
 
 // ForcedDelete 强制删除数据
@@ -498,7 +553,7 @@ func (a *Action[T]) ForcedDelete(wheres ...Scopemethod) error {
 }
 
 // ForcedDeleteByID 根据ID强制删除数据
-func (a *Action[T]) ForcedDeleteByID(id uint) error {
+func (a *Action[T]) ForcedDeleteByID(id uint, wheres ...Scopemethod) error {
 	ctx := a.ctx
 	if a.enableTrace {
 		_ctx, span := otel.Tracer("gorm-normalize").Start(a.ctx, "ForcedDeleteByID")
@@ -506,5 +561,5 @@ func (a *Action[T]) ForcedDeleteByID(id uint) error {
 		ctx = _ctx
 	}
 	var m T
-	return a.DB().WithContext(ctx).Unscoped().Delete(&m, id).Error
+	return a.DB().WithContext(ctx).Unscoped().Scopes(wheres...).Delete(&m, id).Error
 }
