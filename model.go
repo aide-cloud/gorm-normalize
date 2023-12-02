1f6f676ade1f86bdb14debf8a6e08504512dab34
胡标 <aidecloud@163.com>
2023-12-02 14:21:37 +0800

    调整model映射结构, deletedAt字段错误问题

--- model.go
+++ model.go
@@ -10,5 +10,5 @@ type BaseModel struct {
 	ID        uint                  `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time             `gorm:"column:created_at;type:timestamp;not null;default:CURRENT_TIMESTAMP;comment:创建时间" json:"createdAt"`
 	UpdatedAt time.Time             `gorm:"column:updated_at;type:timestamp;not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;comment:更新时间" json:"updatedAt"`
-	DeletedAt soft_delete.DeletedAt `gorm:"column:created_at;type:bigint;not null;default:0;" json:"deletedAt"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:deleted_at;type:bigint;not null;default:0;" json:"deletedAt"`
 }
