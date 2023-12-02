419b0388bf70ecc82c0c33ac0366533b766bbcfc
胡标 <aidecloud@163.com>
2023-12-02 13:21:29 +0800

    调整model映射结构

--- model.go
+++ model.go
@@ -8,7 +8,7 @@ import (
 
 type BaseModel struct {
 	ID        uint                  `gorm:"primary_key" json:"id"`
-	CreatedAt time.Time             `json:"created_at"`
-	UpdatedAt time.Time             `json:"updated_at"`
-	DeletedAt soft_delete.DeletedAt `json:"deleted_at"`
+	CreatedAt time.Time             `gorm:"column:created_at;type:timestamp;not null;default:CURRENT_TIMESTAMP;comment:创建时间" json:"createdAt"`
+	UpdatedAt time.Time             `gorm:"column:updated_at;type:timestamp;not null;default:CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;comment:更新时间" json:"updatedAt"`
+	DeletedAt soft_delete.DeletedAt `gorm:"column:created_at;type:bigint;not null;default:0;" json:"deletedAt"`
 }
